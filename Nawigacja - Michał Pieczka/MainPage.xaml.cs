df49a55fa0e71004dffc2800a7d9b98250aa5f1b
Michał Pieczka <michalpieczka99@gmail.com>
2021-04-17 12:06:20 +0200

    adding files

---
+++ Nawigacja - Michał Pieczka/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x415
+
+namespace Nawigacja___Michał_Pieczka
+{
+    /// <summary>
+    /// Pusta strona, która może być używana samodzielnie lub do której można nawigować wewnątrz ramki.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void powMapa_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void pomMapa_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void trybMapy_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void koordynaty_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
