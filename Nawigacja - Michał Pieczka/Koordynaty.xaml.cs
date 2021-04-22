2abe4a4e1db91723f299dbd89e7ba153340d65db
Michał Pieczka <michalpieczka99@gmail.com>
2021-04-22 21:12:22 +0200

    new-stuff

--- Nawigacja - Michał Pieczka/Koordynaty.xaml.cs
+++ Nawigacja - Michał Pieczka/Koordynaty.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 using Windows.Devices.Geolocation;
+using Windows.Services.Maps;
 
 //Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -30,6 +31,9 @@ namespace Nawigacja___Michał_Pieczka
             gdzieJaNaMapie();
         }
 
+        BasicGeoposition pktStart = new BasicGeoposition();
+        BasicGeoposition pktKoniec = new BasicGeoposition();
+
         private void btnPowrot_Click(object sender, RoutedEventArgs e)
         {
             Frame.GoBack();
@@ -41,7 +45,44 @@ namespace Nawigacja___Michał_Pieczka
                 DesiredAccuracy = PositionAccuracy.High
             };
             Geoposition mojeZGPS = await mojGPS.GetGeopositionAsync();
-            tbGPS.Text = "długość: " + mojeZGPS.Coordinate.Point.Position.Longitude + "; szerokosc: " + mojeZGPS.Coordinate.Point.Position.Latitude;
+
+            pktStart.Latitude = mojeZGPS.Coordinate.Point.Position.Latitude;
+            pktStart.Longitude = mojeZGPS.Coordinate.Point.Position.Longitude;
+            DaneGeograficzne.pktStartowy = pktStart;
+
+            tbGPS.Text = "długość: " + mojeZGPS.Coordinate.Point.Position.Longitude.ToString() + "; szerokosc: " + mojeZGPS.Coordinate.Point.Position.Latitude.ToString();
         }
+        async private void btnSzukaj_Click(object sender, RoutedEventArgs e)
+        {
+            
+            var textCelu = tbCel.Text;
+            if (textCelu != "")
+            {
+                Geopoint hintPoint = new Geopoint(pktStart);
+                MapLocationFinderResult result =
+                    await MapLocationFinder.FindLocationsAsync(textCelu, hintPoint, 3);
+
+                var dl_geogr = result.Locations[0].Point.Position.Longitude;
+                var szer_geogr = result.Locations[0].Point.Position.Latitude;
+
+                pktKoniec.Latitude = szer_geogr;
+                pktKoniec.Longitude = dl_geogr;
+
+                DaneGeograficzne.pktDocelowy = pktKoniec;
+
+                if (result.Status == MapLocationFinderStatus.Success)
+                {
+                    tbDlug_cel.Text = "Długość geograficzna: " +
+                        dl_geogr;
+                    tbSzer_cel.Text = "Szerokość geograficzna: " +
+                        szer_geogr;
+                }
+            }
+            else
+            {
+                tbCel.Text = "Wprowadź poprawne dane lokalizacyjne";
+            }
+        }
+
     }
 }
--- Nawigacja - Michał Pieczka/MainPage.xaml.cs
+++ Nawigacja - Michał Pieczka/MainPage.xaml.cs
@@ -14,6 +14,10 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Controls.Maps;
 using Windows.UI.Xaml.Navigation;
 
+using Windows.Devices.Geolocation;
+using Windows.Services.Maps;
+using Windows.ApplicationModel.Core;
+
 //Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x415
 
 namespace Nawigacja___Michał_Pieczka
@@ -26,6 +30,46 @@ namespace Nawigacja___Michał_Pieczka
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        async protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (DaneGeograficzne.pktStartowy.Latitude != 0 && DaneGeograficzne.pktDocelowy.Longitude != 0)
+            {
+                Geopoint pktStartowy = new Geopoint(DaneGeograficzne.pktStartowy);
+                Geopoint pktDocelowy = new Geopoint(DaneGeograficzne.pktDocelowy);
+
+                MapIcon znacznikStart = new MapIcon();
+                znacznikStart.Location = pktStartowy;
+                znacznikStart.Title = "Tu jestem";
+
+                MapIcon znacznikDocelowa = new MapIcon();
+                znacznikDocelowa.Location = pktDocelowy;
+                znacznikDocelowa.Title = "Koniec!";
+
+                MapPolyline trasaLotem = new MapPolyline();
+                trasaLotem.StrokeColor = Windows.UI.Colors.Black;
+                trasaLotem.StrokeThickness = 3;
+                trasaLotem.StrokeDashed = true;
+
+                var Path = new Geopath(new List<BasicGeoposition>{
+                    DaneGeograficzne.pktStartowy,
+                    DaneGeograficzne.pktDocelowy
+                });
+
+                mojaMapa.MapElements.Add(znacznikStart);
+                mojaMapa.MapElements.Add(znacznikDocelowa);
+                mojaMapa.MapElements.Add(trasaLotem);
+
+                mojaMapa.Center = pktStartowy;
+
+                await mojaMapa.TrySetViewAsync(new Geopoint(DaneGeograficzne.pktStartowy), 8);
+
+            }
+            else
+            {
+                //CoreApplication.Exit();
+            }
         }
 
         private void powMapa_Click(object sender, RoutedEventArgs e)
@@ -65,5 +109,8 @@ namespace Nawigacja___Michał_Pieczka
         {
             Frame.Navigate(typeof(Koordynaty));
         }
+
+
+        
     }
 }
